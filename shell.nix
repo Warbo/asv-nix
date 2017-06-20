d49369754b966c31e350f148da805895a6c12532
Chris Warburton <chriswarbo@gmail.com>
2017-06-20 01:34:49 +0100

    Allow nixpkgs to override nix-config for asv

--- shell.nix
+++ shell.nix
@@ -1,10 +1,14 @@
 with rec {
-  oldCfg = (import <nixpkgs> {}).fetchgit {
+  origPkgs = import <nixpkgs> {};
+
+  oldCfg = origPkgs.fetchgit {
     url    = "http://chriswarbo.net/git/nix-config.git";
     rev    = "c1530a0";
     sha256 = "01zd9g09h0zmx6af927z7j71yyqvnd09hzlg2r5pcj2rrw8kqs7j";
   };
 
   oldPkgs = import <nixpkgs> { config = import "${oldCfg}/config.nix"; };
+
+  pkgs = if origPkgs ? asv then origPkgs else oldPkgs.latestCfgPkgs;
 };
-oldPkgs.latestCfgPkgs.callPackage ./. {}
+pkgs.callPackage ./. {}
