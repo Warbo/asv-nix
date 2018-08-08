249f560d3f614ba57811462b9a1753bea7d2fee1
Chris Warburton <chriswarbo@gmail.com>
2018-08-08 16:01:15 +0100

    Pull example out of derivation

--- derivation.nix
+++ derivation.nix
@@ -1,16 +1,7 @@
-{ asv, callPackage, pythonPackages, withDeps, withNix }:
+{ pythonPackages }:
 
-with rec {
-  # The actual Python package
-  raw = pythonPackages.buildPythonPackage {
-    name = "asv-nix";
-    src  = ./python;
-  };
-
-  # Constructs an example project and tests that everything hooks together
-  example = callPackage ./example.nix { inherit asv raw withNix; };
-};
-
-# Export the Python package, but add the example as a dependency so that the
-# build will abort if it doesn't work
-withDeps [ example ] raw
+pythonPackages.buildPythonPackage {
+  name = "asv-nix";
+  src  = ./python;
+  buildInputs = [ pythonPackages.asv ];
+}
